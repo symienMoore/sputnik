1e2ea68d955cdd13a3a20437e27876e7c9b25b47
symien moore <symien.moore@gmail.com>
2022-06-29 07:24:36 -0400

    initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import { trpc } from "../utils/trpc";
+
+const Home: NextPage = () => {
+  const hello = trpc.useQuery(["example.hello", { text: "from tRPC" }]);
+
+  return (
+    <>
+      <h3>Sputnik</h3>
+    </>
+  );
+};
+
+export default Home;
